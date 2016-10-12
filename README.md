21bf7806e693848b6a52648763b0cb925bf33524
mochida.akihiro <mochida.akihiro@Donuts-N.local>
2016-10-12 16:41:38 +0900

    add README and goemon

---
+++ README.md
@@ -0,0 +1,12 @@
+
+# README
+
+## 環境構築
+
+```
+$ git clone https://github.com/ilovemochida/sign_up_go.git
+$ go get github.com/mattn/goemon/cmd/goemon
+$ goemon go run main.go
+
+```
+
--- main.go
+++ main.go
@@ -22,8 +22,22 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func signUpHandler(w http.ResponseWriter, r *http.Request) {
+	page := Page{"sign up page"}
+	tmpl, err := template.ParseFiles("./view/signUp.html")
+	if err != nil {
+		panic(err)
+	}
+
+	err = tmpl.Execute(w, page)
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
 	http.Handle("/public/", http.StripPrefix("/public/", http.FileServer(http.Dir("public"))))
 	http.HandleFunc("/", viewHandler)
+	http.HandleFunc("/sign_up", signUpHandler)
 	http.ListenAndServe(":8080", nil)
 }
